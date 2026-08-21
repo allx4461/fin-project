15dce050daf1f4ae44e6b36ecf01a3dabdff5794
allx4461 <akuzmina4461@gmail.com>
2026-08-21 16:56:16 +0300

    проба пера

---
+++ scripts/explore-data.py
@@ -0,0 +1,18 @@
+from datasets import load_dataset
+import time
+
+print("connecting to HF")
+
+ds = load_dataset("Zihan1004/FNSPID", "full", split="train", streaming=True)#!!!streaming=True
+
+print("connected succesfully")
+start = time.time()
+
+for i, row in enumerate(ds):
+    print("\n--- columns ---")
+    print(row.keys())
+    print("\n--- 1st row ---")
+    print(row)
+    break
+    
+print(f"\ntime: {time.time() - start:.2f} s")
