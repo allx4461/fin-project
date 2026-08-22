28cc6d4dc91db24803213c1865b4e03ed1597861
allx4461 <akuzmina4461@gmail.com>
2026-08-22 13:02:05 +0300

    nvda exp

---
+++ scripts/nvda_details.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from pathlib import Path
+import datetime
+PROJECT_ROOT = Path(__file__).parent.parent
+DATA_PATH = PROJECT_ROOT / "data" / "raw" / "nasdaq_exteral_data.csv"
+
+chunks=pd.read_csv(DATA_PATH,chunksize=500)
+
+def find_only_nvda(chunks)->pd.DataFrame:
+    nvda_news_list=[]
+    for chunk in chunks:
+        nvda_chunk=chunk[chunk['Stock symbol']=='NVDA']
+        nvda_news_list.append(nvda_chunk)
+    nvda_news=pd.concat(nvda_news_list)
+    return nvda_news
+def nvda_news_details(news:pd.DataFrame)->str:
+    length=len(news)
+    oldest=news['Date'].min()#должно сработать как мин строка
+    newest=news['Date'].max()
+    return f'resulted w {length} nvda news dated from {oldest} to {newest}\n '
+print(nvda_news_details(find_only_nvda(chunks)))
