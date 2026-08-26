b10737723524ea6b735bb9eed284653fe7142882
allx4461 <akuzmina4461@gmail.com>
2026-08-27 01:40:38 +0300

    i

--- scripts/get_graph_best_models.py
+++ scripts/get_graph_best_models.py
@@ -4,20 +4,20 @@ from train_catboost import FEATURE_SETS
 import pandas as pd
 from pathlib import Path
 import sys
-
+from train_linear import FEATURE_SETS
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
 sys.path.append(str(PROJECT_ROOT))
 
 def get_graph(output_path):
     data_path = PROJECT_ROOT / "data" / "processed" / "nvda_features.csv"
     df = pd.read_csv(data_path)
-    val_pred_linear,test_pred_linear=train_linear.train_model(df,'price_roberta')
+    val_pred_linear,test_pred_linear=train_linear.train_model(df,FEATURE_SETS['price_roberta'])
     pred_linear=pd.concat(val_pred_linear,test_pred_linear,ignore_index=True)
 
-    val_pred_forest,test_pred_forest=train_forest.train_model(df,'all_features')
+    val_pred_forest,test_pred_forest=train_forest.train_model(df,FEATURE_SETS['all_features'])
     pred_forest=pd.concat(val_pred_forest,test_pred_forest,ignore_index=True)
 
-    val_pred_catboost,test_pred_catboost=train_catboost.train_model(df,0.05,8,1.0,'all_features')
+    val_pred_catboost,test_pred_catboost=train_catboost.train_model(df,0.05,8,1.0,FEATURE_SETS['all_features'])
     pred_catboost=pd.concat(val_pred_catboost,test_pred_catboost,ignore_index=True)
 
     all_preds = pd.concat([pred_linear, pred_forest, pred_catboost], ignore_index=True)
