5aed76c7e2d581a9b768eb6a53e48049ffba957d
allx4461 <akuzmina4461@gmail.com>
2026-09-03 16:38:08 +0300

    i

--- scripts/train_all.py
+++ scripts/train_all.py
@@ -7,8 +7,11 @@ sys.path.append(str(PROJECT_ROOT))
 import scripts.train_linear as train_linear
 import scripts.train_forest as train_forest
 import scripts.train_catboost as train_catboost
-
+import scripts.mean_baseline as get_mean
+import scripts.zero_baseline as get_zero
 if __name__ == '__main__':
+    get_mean.start()
+    get_zero.start()
     train_linear.start()
     train_forest.start()
     train_catboost.start()
